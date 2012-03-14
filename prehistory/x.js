c2b9655fd8890ae6a74f19edf3d527ec605c1238
Douglas Bagnall <douglas@paradise.net.nz>
2012-03-14 21:11:57 +1300

    old version -- called '0_5_7.js'

--- prehistory/x.js
+++ prehistory/x.js
@@ -7,15 +7,16 @@ strink=''
 html='<table cellpadding=4 class=t><tr>'
 x='g00000000g'         //g stands for off board
 y='gggggggggg'
-bstring=y+y+"g23456432gg11111111g"+x+x+x+x+"g99999999ggABCDECBAg"+y+y  //in base 26 (g is 16)
+bstring=y+y+"g468ac864gg22222222g"+x+x+x+x+"g33333333gg579bd975g"+y+y  //in base 26 (g is 16)
 wstring=x+x+x+"000111100000123321000123553210"
-moves=[0,0,[1,10],[21,19,12,8],[11,9],[1,10,11,9],[1,10,9,11],0]
+m=[0,0,[1,10],[21,19,12,8],[11,9],[1,10,11,9],[1,10,9,11],0]
+moves=[]
 //pawn=[8,48]
-castle=[3,3]//castle[0] &1 is white's left, castle[0]&2 is right castle
+castle=[3,3] //castle[0] &1 is white's left, castle[0]&2 is right castle
 ky=[20,90]
 kx=[5,5]
 kp=[25,95]
-pv=[0,1,5,3,3,9,63,0]
+pv=[0,0,1,1,5,5,3,3,3,3,9,9,63,63,0,0]
 pn=[]
 weight=[]
 ep=0  //en passant state off
@@ -25,15 +26,15 @@ M3=1    // line up with king weight
 beta=[0,0,0,0,0,0]
 parsees=prunees=evaluees=0
 
-for(z=0;z<8;){
-    pv[z+8]=pv[z]=10*pv[z]  //same piece values for black and white
-    if(moves[z]){
-        s=moves[z].length           //probably some better way
+for(z=0;z<16;){
+    pv[z]=10*pv[z]  //same piece values for black and white
+    if(m[z>>1]){
+        s=m[z].length           //probably some better way
+        moves[z]=m[z]
         for(x=0;x<s;){
-            moves[z][s+x]=-moves[z][x++]
+            moves[z][s+x]=-m[z][x++]
         }
     }
-    moves[z+8]=moves[z++]          //both refer to same array
 }
 
 closeness=[0,0,[],[],[],[],[]]
@@ -58,20 +59,14 @@ for (y=110;y>=0;y-=10){
         weight[z]=parseInt(wstring.charAt(((z<60)?z:119-z)),26)
         w=board[z]=parseInt(bstring.charAt(z),26)
         if(w<16){
-            html+=('<td class=' + ((x+(y/10))&1?'b':'w') + '><a href="#" onclick="B('+z+')"><img src='+board[z]+'.gif width=44 height=44 name=i'+z+' border=0></a></td>\n')
+                html+=('<td class=' + ((x+(y/10))&1?'b':'w') + '><a href="#" onclick="B('+z+')"><img src='+board[z]+'.gif width=44 height=44 name=i'+z+' border=0></a></td>\n')
+            }
+            else html+='<td class=h>'+(y-10)/10+'</td>'
         }
-		else{
-			if(y%110) html+=('<td class=h>&nbsp;</td>')
-//            else{ if(x%9==0 &&y%110)html+='<td class=h>'+(y-10)/10+'</td>'
-//                  else html+=('<td class=g><img src='+((z%6)+1+8*(y>50))+'.gif></td>')
-//            }
-    	}
-//		else{
-//			if(y%90==10) html+=('<td class=h>'+lttrs.charAt(x-1)+'</td>')
-//            else{ if(x%9==0 &&y%110)html+='<td class=h>'+(y-10)/10+'</td>'
-//                  else html+=('<td class=g><img src='+((z%6)+1+8*(y>50))+'.gif></td>')
-//            }
-//    	}
+        else{
+            if(y%90==10) html+=('<td class=h>'+lttrs.charAt(x-1)+'</td>')
+            else  html+=('<td class=g><img src='+((z%6)+1+8*(y>50))+'.gif></td>')
+        }
     }
     html+='</tr><tr>\n'
 }
@@ -82,36 +77,35 @@ html+='</tr></table>'
 
 //**************************************************functions
 // tyx==position+ move (temp yx)
-// bmx== blackness flag
+// bm== blackness flag
 // k
-// yx,h,aa,a,cx,mv,k=-1,bmx=bm>>3,dir=bmx*10-20,mvl=[],mvlength,wate
+// yx,h,aa,a,cx,mv,k=-1,bm=bm>>3,dir=bm*10-20,mvl=[],mvlength,wate
 
 function parse(bm,EP,tpn){
 //	var bord=board
-    var tyx,yx,h,aa,a,cx,mv,k=-1,bmx=bm>>3,nbm=bm^8,nx=nbm>>3,dir=10-bmx*20,mvl=[],m,wate,cbmx=castle[bmx]
+    var tyx,yx,h,aa,a,cx,mv,k=-1,nbm=1-bm,dir=10-bm*20,mvl=[],m,wate,cbm=castle[bm]
     for(yx=21;yx<99;yx++){
 		a=board[yx]
-        if(a&15&&bm==(a&8)){
+        if(a&15&&bm==(a&1)){
             wate=weight[yx]*M2
-            a&=7
-            if(a>1){    //non-pawns
-            	ic=(closeness[a][40+kp[nx]-yx]||0)//initial closeness
-                ak=a==6
+            a&=14
+            if(a>2){    //non-pawns
+            	ic=(closeness[a][40+kp[nbm]-yx]||0)//initial closeness
+                ak=a==12
                 mv=moves[a]
-                if(a==3||ak){
+                if(a==6||ak){
                    for(cx=0;cx<8;){     //knights,kings
                    		tyx=yx+mv[cx++]
                         aa=board[tyx]
-                        if(!aa||(aa&24)==nbm){
-                            mvl[++k]=[tpn+pv[aa]+(ak?0:weight[tyx]*M1)-wate+(ak&&moveno<30?(closeness[a][40+kp[nx]-tyx]||0)*M3-ic:0),yx,tyx] //rating,start,end,-- enpassant left undefined
-                            //if ((aa&7)==6)return 0
+                        if(!aa||(aa&17)==nbm){
+                            mvl[++k]=[tpn+pv[aa]+(ak?0:weight[tyx]*M1)-wate+(ak&&moveno<30?(closeness[a][40+kp[nbm]-tyx]||0)*M3-ic:0),yx,tyx] //rating,start,end,-- enpassant left undefined
                         }
                     }
-                    if(ak&&cbmx){
-                        if(cbmx&1&&!(board[yx-1]+board[yx-2]+board[yx-3])&&check(yx-2,nbm,dir,-1)){//Q side
+                    if(ak&&cbm){
+                        if(cbm&1&&!(board[yx-1]+board[yx-2]+board[yx-3])&&check(yx-2,nbm,dir,-1)){//Q side
                             mvl[++k]=[tpn+11,yx,yx-2]                                //no analysis, just encouragement
                         }
-                        if(cbmx&2&&!(board[yx+1]+board[yx+2])&&check(yx,nbm,dir,1)){//K side
+                        if(cbm&2&&!(board[yx+1]+board[yx+2])&&check(yx,nbm,dir,1)){//K side
                             mvl[++k]=[tpn+12,yx,yx+2]                                //no analysis, just encouragement
                         }
                     }
@@ -125,8 +119,8 @@ function parse(bm,EP,tpn){
                         while(!aa){   //while on board && no piece
                             tyx+=m
                             aa=board[tyx]
-                            if(!aa||(aa&24)==nbm){
-                                mvl[++k]=[tpn+pv[aa]+(weight[tyx]*M1)-wate+(closeness[a][40+kp[nx]-tyx]||0)*M3-ic,yx,tyx]
+                            if(!aa||(aa&17)==nbm){
+                                mvl[++k]=[tpn+pv[aa]+(weight[tyx]*M1)-wate+(closeness[a][40+kp[nbm]-tyx]||0)*M3-ic,yx,tyx]
                                 //if ((aa&7)==6)return 0
                             }
                         }
@@ -144,12 +138,12 @@ function parse(bm,EP,tpn){
                         mvl[++k]=[tpn+weight[tyx+dir]*M1-wate,yx,tyx+dir,tyx]    //ep points to the takeable spot
                     }
                 }
-                if(EP&&(EP==tyx+1||EP==tyx-1)&& bm!=(board[EP-dir]&8)) {       //enpassant
+                if(EP&&(EP==tyx+1||EP==tyx-1)&& bm!=(board[EP-dir]&1)) {       //enpassant
                     mvl[++k]=[tpn+1+weight[tyx]*M1-wate,yx,EP]
                 }
                 for(h=-1;h<2;h+=2){                        //h=-1,1 --for pawn capturing
                     aa=board[tyx+h]
-                    if(aa&15 && (aa&8)!=bm){
+                    if(aa&15 && (aa&1)!=bm){
                         mvl[++k]=[tpn+pv[aa]+weight[tyx]*M1-wate,yx,tyx+h]
                         //if(aa&7==6)return 0
                     }
@@ -168,7 +162,7 @@ function parse(bm,EP,tpn){
 var comp=new Function('a','b','return b[0]-a[0]')//comparison function for treeclimb integer sort (descending)
 
 function treeclimber(count,bm,tpn,s,e,EP,pruner){
-    var z,mvl,pl,mvt,mv,b
+    var z,mvl,pl,mvt,mv,b,cmp=comp
 //    var cmp=comp
     var a=board[s],aa=board[e],mz=[-999]
     board[e]=a
@@ -190,7 +184,7 @@ function treeclimber(count,bm,tpn,s,e,EP,pruner){
 			}
         }
         else{
-            mvl.sort(comp)//descending order
+            mvl.sort(cmp)//descending order
             b=beta[count]
 			beta[--count]=999
             for(z=0;z<pl;z++){
@@ -229,18 +223,18 @@ function check(yx,nbm,dir,side){         //dir is dir
     for(;yx<ex;yx++){ //go thru 3positions, checking for check in each
         for(m=dir-2;++m<md;){
 			aa=board[yx+m]
-            if(aa&&(aa&8)==nbm&&((aa&7)==1||(aa&7)==6))return 0        //don't need to check for pawn position --cannot arrive at centre without passing thru check
+            if(aa&&(aa&1)==nbm&&((aa&14)==2||(aa&14)==12))return 0        //don't need to check for pawn position --cannot arrive at centre without passing thru check
             aa=0
             tyx=yx
             while(!aa){   //while on board && no piece
                 tyx+=m
                 aa=board[tyx]
                 if (aa&16)break
-                if((aa==nbm+2+(m==dir)*2)||aa==nbm+5)return 0
+                if((aa==nbm+2+(m==dir)*2)||aa==nbm+10)return 0
             }
         }
         for (z=0;z<8;){
-            if(board[yx+moves[3][z++]]-nbm==3)return 0      //knights
+            if(board[yx+moves[3][z++]]-nbm==6)return 0      //knights
         }
     }
     aa=0
@@ -248,7 +242,7 @@ function check(yx,nbm,dir,side){         //dir is dir
 	while (!aa){   //queen or rook out on other side
 		yx-=side
 		aa=board[yx]
-		if(aa==nbm+2||aa==nbm+5)return 0
+		if(aa==nbm+4||aa==nbm+10)return 0
 	}
 	return 1
 }
@@ -282,7 +276,7 @@ function findmove(){
 
 // move the piece (no testing)
 function move(s,e,queener,score){
-    var a=board[s]&7,bmx=bmove>>3,dir=10-bmx*20,aa=board[e],x=s%10,tx=e%10,ty=e-tx,gap=e-s
+    var a=board[s]&7,bm=bmove>>3,dir=10-bm*20,aa=board[e],x=s%10,tx=e%10,ty=e-tx,gap=e-s
     ep=0
     if(a==1){ //pawns
 //     	debug(board[e+dir],"piece:",5-queener,'e',e,'s',s)
@@ -290,20 +284,20 @@ function move(s,e,queener,score){
         if(e==s+2*dir)ep=s+dir       //set up ep  - perhaps do pawn test (save time in loop)
         if(!aa&&x!=tx)shift(e,e+dir)// blank ep pawn
     }
-    if(s==21+bmx*70||s==28+bmx*70)castle[bmx]&=(x<5)+1        //castle flags
+    if(s==21+bm*70||s==28+bm*70)castle[bm]&=(x<5)+1        //castle flags
     if(a==6){
         if(gap*gap==4){  //castling - move rook too
             shift(s-4+(s<e)*7,s+gap/2)
         }
-        castle[bmx]=0
-        kx[bmx]=tx
-        ky[bmx]=ty
-        kp[bmx]=e
+        castle[bm]=0
+        kx[bm]=tx
+        ky[bm]=ty
+        kp[bm]=e
     }
 
     shift(s,e)
     display2(s,e,score,aa)
-    moveno+=bmx
+    moveno+=bm
     if(!(moveno%3)){   //alter strategy multipliers
         M1=(M1-1||1)
         M2=(M2-1||1)
