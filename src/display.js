55d8e751acc36674ffe64dd1ac9ab6623995ab84
Douglas Bagnall <douglas@paradise.net.nz>
2012-03-21 01:06:55 +1300

    break display config into its own file

--- src/display.js
+++ src/display.js
@@ -18,30 +18,6 @@ var input = {
 };
 
 var PROMOTIONS = ['queen', 'rook', 'knight', 'bishop'];
-var ROTATE_BOARD = true;
-var LEVELS = ['stupid', 'middling', 'slow', 'slowest'];
-var SQUEEZE_INTO_BOX = true;
-
-var IMAGE_NAMES = [
-    'images/empty.gif',
-    'images/empty.gif',   // 1 is unused
-    'images/white_pawn.gif',
-    'images/black_pawn.gif',
-    'images/white_rook.gif',
-    'images/black_rook.gif',
-    'images/white_knight.gif',
-    'images/black_knight.gif',
-    'images/white_bishop.gif',
-    'images/black_bishop.gif',
-    'images/white_king.gif',
-    'images/black_king.gif',
-    'images/white_queen.gif',
-    'images/black_queen.gif'
-];
-
-var SQUARE_WIDTH = 30;
-var SQUARE_HEIGHT = 30;
-
 
 function square_clicked(square){
     var state = input.board_state;
@@ -399,8 +375,6 @@ write_board_html();
 write_controls_html();
 if (SQUEEZE_INTO_BOX)
     squeeze_into_box();
-
-
 
 refresh(0);
 next_move();
