8dc7c01abc119c5bdd9e0a883aa2ea680d1f89ab
Douglas Bagnall <douglas@paradise.net.nz>
2012-05-05 20:42:46 +1200

    Add a null-move heuristic using treeclimber
    It is likely that I have misinterpreted the concept, but nevertheless
    it seems to work.

--- src/parse-test.js
+++ src/parse-test.js
@@ -54,7 +54,8 @@ function p4_make_move(state, s, e, castle_state, promotion){
     if (castle_state)
         castle_state &= p4_get_castles_mask(s, e, moved_colour);
 
-    piece_locations.push([end_piece, e]);
+    if (piece)
+        piece_locations.push([end_piece, e]);
     return {
         castle_state: castle_state,
         undo: function(){
@@ -68,7 +69,8 @@ function p4_make_move(state, s, e, castle_state, promotion){
             }
             board[s] = S;
             board[e] = E;
-            piece_locations.length--;
+            if (piece)
+                piece_locations.length--;
         },
         ep: ep
     };
@@ -131,7 +133,75 @@ function p4_negamax_treeclimber(state, count, colour, score, s, e, alpha, beta,
 
 
 
-/****treeclimber */
+function p4_nullmove_alphabeta_treeclimber(state, count, colour, score, s, e, alpha, beta,
+                                           castle_state, promotion){
+    var move = p4_make_move(state, s, e, castle_state, promotion);
+    castle_state = move.castle_state;
+    var i;
+    var ncolour = 1 - colour;
+    var movelist = p4_parse(state, colour, move.ep, castle_state, -score);
+    var movecount = movelist.length;
+    var mv;
+    if(count){
+        //branch nodes
+        var t;
+        /* first try a null move. This sets a decent value for beta in
+           almost every situation (I think. seems to work).
+        */
+        if (count > 1){
+            t = p4_treeclimber(state, 1, colour, score, 0, 0,
+                               alpha, beta, castle_state);
+            //console.log('count, t, alpha, beta', count, t, alpha, beta);
+            if (t < beta)
+                beta = t;
+        }
+        for(i = 0; i < movecount; i++){
+            mv = movelist[i];
+            var mscore = mv[0];
+            var ms = mv[1];
+            var me = mv[2];
+            if (mscore > P4_WIN){ //we won! Don't look further.
+                alpha = P4_KING_VALUE + 200 * count;
+                break;
+            }
+            t = -p4_treeclimber(state, count - 1, ncolour, mscore, ms, me,
+                                -beta, -alpha, castle_state);
+            if (t > alpha){
+                alpha = t;
+            }
+            if (alpha >= beta){
+                break;
+            }
+        }
+
+        if (alpha < -P4_WIN_NOW){
+            /* Whatever we do, we lose the king.
+             *
+             * But is it check?
+             * If not, this is stalemate, and the score doesn't apply.
+             */
+            if (! p4_check_check(state, colour)){
+                alpha = state.stalemate_scores[colour];
+            }
+        }
+        if (alpha < -P4_WIN){
+            /*make distant checkmate seem less bad */
+            alpha += P4_WIN_DECAY;
+        }
+    }
+    else{
+        //leaf nodes
+        while(beta > alpha && --movecount != -1){
+            if(movelist[movecount][0] > alpha){
+                alpha = movelist[movecount][0];
+            }
+        }
+    }
+    move.undo();
+    return alpha;
+}
+
+
 function p4_alphabeta_treeclimber(state, count, colour, score, s, e, alpha, beta,
                                   castle_state, promotion){
     var move = p4_make_move(state, s, e, castle_state, promotion);
@@ -258,6 +328,7 @@ function p4_negascout_treeclimber(state, count, colour, score, s, e, alpha, beta
 var TREE_CLIMBERS = [
     'default', p4_treeclimber,
     'negamax', p4_negamax_treeclimber,
+    'nullmove', p4_nullmove_alphabeta_treeclimber,
     'alphabeta', p4_alphabeta_treeclimber,
     'negascout', p4_negascout_treeclimber
 ];
